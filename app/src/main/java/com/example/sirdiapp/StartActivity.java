ed051a9c061ea303e25123733cf8183e3261c94b
tdn1998 <dushyantkumarbehera@gmail.com>
2019-10-09 18:19:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sirdiapp/StartActivity.java
@@ -0,0 +1,34 @@
+package com.example.sirdiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.start_status_color));
+        }
+    }
+}
