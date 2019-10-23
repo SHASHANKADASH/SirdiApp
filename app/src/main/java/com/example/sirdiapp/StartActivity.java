3a9a84362870b10e5910ed200e31807f5d39f497
tdn1998 <dushyantkumarbehera@gmail.com>
2019-10-24 01:37:04 +0530

    overridden api removed in start activity and new styles added in manifest

--- app/src/main/java/com/example/sirdiapp/StartActivity.java
+++ app/src/main/java/com/example/sirdiapp/StartActivity.java
@@ -23,12 +23,5 @@ public class StartActivity extends AppCompatActivity {
                 finish();
             }
         },1000);
-
-        if (android.os.Build.VERSION.SDK_INT >= 22) {
-            Window window = this.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.green4));
-        }
     }
 }
