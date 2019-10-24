f3696b94caa68c596f2dc24b56011cd3a2e40e58
tdn1998 <dushyantkumarbehera@gmail.com>
2019-10-24 20:31:36 +0530

    bottom nav bar and nav drawer completed (only ui and frag config)

--- app/src/main/java/com/example/sirdiapp/MainActivity.java
+++ app/src/main/java/com/example/sirdiapp/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.sirdiapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bot_nav;
-    Fragment select_frag;
+    private BottomNavigationView bot_nav;
+    private Toolbar toolbar;
+
+    private DrawerLayout nav_drawer;
+    private NavigationView nav_view;
+    private ActionBarDrawerToggle toggle;
 
     private long backpressedtime;
     private Toast backtoast;
@@ -24,36 +30,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         bot_nav=findViewById(R.id.bottom_navigation);
-        open_fragments();
+        open_fragments_bottom_nav();
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new HomeFragment()).commit();
+        nav_drawer=findViewById(R.id.drawer_layout);
+        nav_view=findViewById(R.id.drawer_view);
+        open_fragments_drawer_nav();
+
+        toggle=new ActionBarDrawerToggle(this,nav_drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        nav_drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            //nav_view.setCheckedItem(R.id.drawer_home);
+        }
     }
 
-    private void open_fragments() {
+    private void open_fragments_drawer_nav() {
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch(menuItem.getItemId()){
+                    case R.id.drawer_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new HomeFragment()).commit();
+                        break;
+                    case R.id.drawer_Profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new ProfileFragment()).commit();
+                        break;
+                    case R.id.drawer_notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new NotificationFragment()).commit();
+                        break;
+                    case R.id.drawer_setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new SettingFragment()).commit();
+                        break;
+                }
+                nav_drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    private void open_fragments_bottom_nav() {
         bot_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                select_frag=null;
-
                 switch(menuItem.getItemId()){
                     case R.id.bottom_home:
-                        select_frag=new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new HomeFragment()).commit();
                         break;
                     case R.id.bottom_notification:
-                        select_frag=new NotificationFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new NotificationFragment()).commit();
                         break;
                     case R.id.bottom_Profile:
-                        select_frag=new ProfileFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new ProfileFragment()).commit();
                         break;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        select_frag).commit();
-
                 return true;
             }
         });
@@ -61,14 +106,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if(backpressedtime+2000>System.currentTimeMillis()){
-            backtoast.cancel();
-            super.onBackPressed();
-            return;
+
+        if(nav_drawer.isDrawerOpen(GravityCompat.START)){
+            nav_drawer.closeDrawer(GravityCompat.START);
         } else{
-            backtoast=Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT);
-            backtoast.show();
+            if(backpressedtime+2000>System.currentTimeMillis()){
+                backtoast.cancel();
+                super.onBackPressed();
+                return;
+            } else{
+                backtoast=Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT);
+                backtoast.show();
+            }
+            backpressedtime= System.currentTimeMillis();
         }
-        backpressedtime= System.currentTimeMillis();
     }
 }
