9e1a0ca87183cd990a93e1509bcd4d0a29eee9f4
tdn1998 <dushyantkumarbehera@gmail.com>
2019-10-27 16:17:49 +0530

    start anim

--- app/src/main/java/com/example/sirdiapp/StartActivity.java
+++ app/src/main/java/com/example/sirdiapp/StartActivity.java
@@ -8,12 +8,25 @@ import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        YoYo.with(Techniques.FadeInUp)
+                .duration(2000)
+                .repeat(0)
+                .playOn(findViewById(R.id.start_text));
+
+        YoYo.with(Techniques.FadeInUp)
+                .duration(2000)
+                .repeat(0)
+                .playOn(findViewById(R.id.start_logo));
 
         new Handler().postDelayed(new Runnable() {
             @Override
