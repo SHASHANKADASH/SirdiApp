cf83c4e15ec3a646929c59e962b7d72df89fcaee
tdn1998 <dushyantkumarbehera@gmail.com>
2019-11-12 00:24:28 +0530

    nothing much

--- app/src/main/java/com/example/sirdiapp/LoginActivity.java
+++ app/src/main/java/com/example/sirdiapp/LoginActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
